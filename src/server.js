6aab52383357951d5e123f6b78270ed871c00b24
Quoc Anh Nguyen <quoc.nguyen@hitachivantara.com>
2022-01-21 01:16:08 +0700

    [DGHOME] Setup deploy #17

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('model-data.json');
 const middlewares = jsonServer.defaults({
-    static: '/'
+    static: '/index.html'
 });
 const port = process.env.PORT || 3001;
 
